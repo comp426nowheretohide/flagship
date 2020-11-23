19ba5f6a928099b37240d354ff9e78481aaecf8f
rupshar <rupshar@live.unc.edu>
2020-11-23 15:39:48 -0800

    update

--- Lobby/lobby.js
+++ Lobby/lobby.js
@@ -32,6 +32,9 @@ let createGame = async function(name, id){
         const result = await axios({
             method: 'put', 
             url:`${base}/newGame/${id}/${name}`,
+            headers: {
+                authorization: `bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjNlNTQyN2NkMzUxMDhiNDc2NjUyMDhlYTA0YjhjYTZjODZkMDljOTMiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZmluYWwtYmFkNmYiLCJhdWQiOiJmaW5hbC1iYWQ2ZiIsImF1dGhfdGltZSI6MTYwNjE3NDczNiwidXNlcl9pZCI6InJWZnZXaTRmSUhoQXo4U3NYblJjSGltNEZIczEiLCJzdWIiOiJyVmZ2V2k0ZklIaEF6OFNzWG5SY0hpbTRGSHMxIiwiaWF0IjoxNjA2MTc0NzM2LCJleHAiOjE2MDYxNzgzMzYsImVtYWlsIjoic2JhaGFsaUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsic2JhaGFsaUBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.xch6HutfmmCpI9DoOD-xtJfAsLRdS4L10HwHk8Zyd-UKev5rg3blItKHL0A_dl5R5O6_qYotR5IMiFyJsaWSkhFQ5UkisaxCUc0ycXLzmWFLStmZOnnNiFCUW-raH5jHo7xz3NN3L-pnY5JaGcLuv-pDd18wyupg9e9u6UDWUEW5B0Bh0-NdvCthgHqMIDiOIi8ajL3QNSd3a7bAKOCODdCPOqWeaACp84Y2R_hufp9TUWuv2MQfNXvtVyqzuj1hj1ox_EXuItSs5GER_AsYvyMQzwe51oh1kFwSojGM53oGN54Ms7Y1cydjnO0KHT2W8mzLxHMX0og2cgAU9ZP58A`,
+            }, 
             withCredentials: true
         })
         return result;
