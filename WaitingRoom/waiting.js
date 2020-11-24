e410cb194f0340a2f3fa076b0dd1a3d51c1aa633
rupshar <rupshar@live.unc.edu>
2020-11-23 19:46:55 -0800

    Removing try/catch block, could be source of issues

--- WaitingRoom/waiting.js
+++ WaitingRoom/waiting.js
@@ -50,28 +50,24 @@ let idToken = sessionStorage.authToken;
 let base = sessionStorage.base;
 
 setInterval(async function() {
-    try{
-        const result = await axios({
-            method: 'get',
-            url: `${base}/lobby/${gameId}`,
-            headers: {
-                authorization: `bearer ${idToken}`
-            },
-            withCredentials: true,
-        })
-        let users = new Array(6)
-        for(let i = 0; i < 6; i++) {
-            users[i] = res.data[i];
-        }
-        for(let i = 0; i < 6; i++) {
-            if(users[i] === undefined) {
-                break
-            } else {
-                document.querySelector(`.user${i+1}`).innerHTML = `Player ${i + 1}: ` + users[i]
-            }
-        }
-        return result;
-    } catch(error) {
-        console.log("Well, this sucks")
+    const result = await axios({
+        method: 'get',
+        url: `${base}/lobby/${gameId}`,
+        headers: {
+            authorization: `bearer ${idToken}`
+        },
+        withCredentials: true,
+    })
+    let users = new Array(6)
+    for(let i = 0; i < 6; i++) {
+        users[i] = res.data[i];
     }
+    for(let i = 0; i < 6; i++) {
+        if(users[i] === undefined) {
+            break
+        } else {
+            document.querySelector(`.user${i+1}`).innerHTML = `Player ${i + 1}: ` + users[i]
+        }
+    }
+    return result;
 }, 3000)
