d9136bdf5c3d95aa618633f0da1f352f4ca0ecd3
rupshar <rupshar@live.unc.edu>
2020-11-23 19:38:07 -0800

    Modified request structure for waiting room

--- WaitingRoom/waiting.js
+++ WaitingRoom/waiting.js
@@ -50,5 +50,28 @@ let idToken = sessionStorage.authToken;
 let base = sessionStorage.base;
 
 setInterval(async function() {
-
-})
+    try{
+        const result = await axios({
+            method: 'get',
+            url: `${base}/lobby/${gameId}`,
+            headers: {
+                authorization: `bearer ${idToken}`
+            },
+            withCredentials: true,
+        })
+        let users = new Array(6)
+        for(let i = 0; i < 6; i++) {
+            users[i] = res.data[i];
+        }
+        for(let i = 0; i < 6; i++) {
+            if(users[i] === undefined) {
+                break
+            } else {
+                document.querySelector(`.user${i+1}`).innerHTML = `Player ${i + 1}: ` + users[i]
+            }
+        }
+        return result;
+    } catch(error) {
+        document.querySelector('.user1').innerHTML = "Well, this sucks"
+    }
+}, 3000)
